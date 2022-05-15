1d537181c872dffca4edace010774559563baa1c
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-15 23:59:45 +0300

    stream/funtional_interface/unary_operator/task

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -1,19 +1,14 @@
 package ru.clevertec;
 
 
-interface BinaryOperator<T> {
-    T apply(T t1, T t2);
+interface UnaryOperator<T> {
+    T apply(T t);
 }
 
 public class Runner {
     public static void main(String[] args) {
 
-        // Умножить два числа с помощью бинарного оператора
+        // Получить квадрат числа с помощью унарного оператора
 
-        BinaryOperator<Integer> multiply = (x, y) -> x * y;
-
-        Integer result = multiply.apply(5, 9);
-
-        System.out.println("result = " + result);
     }
 }
