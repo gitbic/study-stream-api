6bbf3d9ec8afe9fc73b96c3983ed91df46447932
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-15 13:39:40 +0300

    functional_interface/predicate/solution

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -8,4 +8,11 @@ interface Predicate<T> {
 public class Runner {
 
     // Задача: реализовать функциональный интерфейс Predicate для проверки числа, чтобы оно было больше 5
+
+    public static void main(String[] args) {
+        Predicate<Integer> isMoreFive = integer -> integer > 5;
+
+        System.out.println("isMoreFive.test(3) = " + isMoreFive.test(3));
+        System.out.println("isMoreFive.test(7) = " + isMoreFive.test(7));
+    }
 }
