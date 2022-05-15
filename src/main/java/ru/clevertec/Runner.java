98e359af5a18badc78f17ed7f6efc9cf911ffb83
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-15 12:30:15 +0300

    functional_interface/anonimous

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -1,10 +1,24 @@
 package ru.clevertec;
 
+@FunctionalInterface
+interface Greeting {
+    String sayHallo(String name);
+}
 
 public class Runner {
-    public static void main(String[] args) {
 
     // Задача: Создать функциональный интерфейс Greeting, с методом sayHallo который принимает строку "имя" и возвращает строку приветствия
     //         С помощью анонимного класса реализовать интерфейс Greeting
+
+    public static void main(String[] args) {
+
+        Greeting greeting = new Greeting() {
+            @Override
+            public String sayHallo(String name) {
+                return "Hallo " + name;
+            }
+        };
+
+        System.out.println(greeting.sayHallo("Dima"));
     }
 }
