be8fdb9f9d0fce504839aeb4bdd3943d6f956d5d
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-15 15:55:21 +0300

    stream/peek/task

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -10,5 +10,10 @@ public class Runner {
     public static void main(String[] args) {
         List<Product> products = ProductUtils.createProducts();
 
+        //Задача: Вывести на консоль имя продукта и его стоимость
+        //        Для этого написать Consumer отдельно
+        //        Уменьшить цену для товаров на скидке на 10%
+        //        Вывести на консоль имя продукта и его стоимость
+
     }
 }
