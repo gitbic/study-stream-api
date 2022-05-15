f0369abe8d467adf6071e1199f7c45caa9c044e1
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-15 13:33:52 +0300

    functional_interface/predicate/task

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -1,32 +1,11 @@
 package ru.clevertec;
 
 @FunctionalInterface
-interface Greeting {
-    String sayHallo(String name);
+interface Predicate<T> {
+    boolean test(T t);
 }
 
 public class Runner {
 
-    // Задача: Создать функциональный интерфейс Greeting, с методом sayHallo который принимает строку "имя" и возвращает строку приветствия
-    //         С помощью анонимного класса реализовать интерфейс Greeting
-
-    public static void main(String[] args) {
-
-        Greeting greeting = new Greeting() {
-            @Override
-            public String sayHallo(String name) {
-                return "Hallo " + name;
-            }
-        };
-
-        Greeting greeting1 = name -> {
-            return "Hello " + name;
-        };
-
-        Greeting greeting2 = name -> "Hello " + name;
-
-        System.out.println(greeting.sayHallo("Dima"));
-        System.out.println(greeting1.sayHallo("Dima"));
-        System.out.println(greeting2.sayHallo("Dima"));
-    }
+    // Задача: реализовать функциональный интерфейс Predicate для проверки числа, чтобы оно было больше 5
 }
