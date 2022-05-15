641219002bf60bd3e7e76b9354058c0d05c12666
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-15 15:00:11 +0300

    stream/filter/task

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -1,13 +1,19 @@
 package ru.clevertec;
 
-
 import ru.clevertec.dto.Product;
 import ru.clevertec.util.ProductUtils;
 
 import java.util.List;
 
 public class Runner {
+
+// Задача: Найти продукты с ценой выше 5 и вывести на консоль
+// Используя анонимный класс
+// Используя лямбду
+// Используя предикат
+
     public static void main(String[] args) {
+
         List<Product> products = ProductUtils.createProducts();
 
     }
