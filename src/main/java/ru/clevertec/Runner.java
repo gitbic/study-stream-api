e9ef3bf46d35adc4d72687c1d47cddcb361d1866
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-16 00:15:26 +0300

    stream/functional_interface/supplier/solution

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -1,12 +1,20 @@
 package ru.clevertec;
 
+import java.time.LocalTime;
+
 interface Supplier<T> {
     T get();
 }
 public class Runner {
     public static void main(String[] args) {
 
-        // Задача сгенерировать текущую дату и время при помощи интерфейса supplier
+        // Задача сгенерировать текущее время при помощи интерфейса Supplier
+
+        Supplier<LocalTime> time = LocalTime::now;
+
+        LocalTime localTime = time.get();
+
+        System.out.println("localTime = " + localTime);
 
     }
 }
