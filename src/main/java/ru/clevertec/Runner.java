8f9ce84f64b6ff4b3dd5b395ddcacda07634779d
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-15 15:28:05 +0300

    functional_interface/consumer/task

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -1,14 +1,13 @@
 package ru.clevertec;
 
-
-import ru.clevertec.dto.Product;
-import ru.clevertec.util.ProductUtils;
-
-import java.util.List;
+interface Consumer<T> {
+    void accept(T t);
+}
 
 public class Runner {
     public static void main(String[] args) {
-        List<Product> products = ProductUtils.createProducts();
+
+        // Реализовать функциональный интерфейс Consumer, который напечатает 100 долларов
 
     }
 }
