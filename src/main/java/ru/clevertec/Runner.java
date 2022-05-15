c974e2ff01501d04f7253c3fa78360899a338bef
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-15 20:52:41 +0300

    stream/collect/task

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -10,5 +10,8 @@ public class Runner {
     public static void main(String[] args) {
         List<Product> products = ProductUtils.createProducts();
 
+        // Задача: Создать новый список с продуктами со скидкой и вывести его на консоль
+        // Создать Новый LinkedHashSet с продуктами с помощью реализации интерфейса Supplier и вывести на консоль
+
     }
 }
