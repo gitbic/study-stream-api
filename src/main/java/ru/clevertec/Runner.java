4df285b9216aaf6aeb40807540d85a0e8ef4298e
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-15 16:05:27 +0300

    stream/peek/solution

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -5,12 +5,26 @@ import ru.clevertec.dto.Product;
 import ru.clevertec.util.ProductUtils;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Runner {
     public static void main(String[] args) {
         List<Product> products = ProductUtils.createProducts();
 
-        //Задача: уменьшить цену для товаров на скидке на 10% и вывести на консоль продукты
+        //Задача: Вывести на консоль имя продукта и его стоимость
+        //        Для этого написать Consumer отдельно
+        //        Уменьшить цену для товаров на скидке на 10%
+        //        Вывести на консоль имя продукта и его стоимость
 
+        Consumer<Product> printedProduct =
+            product -> System.out.println(product.getName() + " cost: " + product.getPrice());
+
+        products.stream()
+            .peek(printedProduct)
+            .peek(product -> {
+                double price = product.getPrice();
+                product.setPrice(price - price / 10);
+            })
+            .forEach(printedProduct);
     }
 }
