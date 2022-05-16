9042a32391ea5854cefd4f7e738e125665c75cd2
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-16 14:26:26 +0300

    stream/min_max/task

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -6,6 +6,11 @@ import ru.clevertec.util.ProductUtils;
 
 import java.util.List;
 
+// Написать свой компаратор для сравнения цены на продукт
+// Найти минимальную цену товара с помощью компаратора и получения первого элемента
+// Найти максимальную цену товара с помощью анонимного класса и лямбды
+
+
 public class Runner {
     public static void main(String[] args) {
         List<Product> products = ProductUtils.createProducts();
