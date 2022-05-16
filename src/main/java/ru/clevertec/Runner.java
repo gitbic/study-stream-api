4edb30619d2516ca39e85a2bc5f45cab2cd934fd
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-16 15:53:09 +0300

    stream/reduce/solution

--- src/main/java/ru/clevertec/Runner.java
+++ src/main/java/ru/clevertec/Runner.java
@@ -2,9 +2,11 @@ package ru.clevertec;
 
 
 import ru.clevertec.dto.Product;
+import ru.clevertec.dto.Shop;
 import ru.clevertec.util.ProductUtils;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Runner {
     public static void main(String[] args) {
@@ -12,5 +14,11 @@ public class Runner {
 
         //        Задача:  Найти все продукты в доступные в магазине Алми и посчитать сколько выйдет в сумме за все доступные продукты
 
+        Optional<Double> sum = products.stream()
+            .filter(product -> product.getMetaInf().getShopList().contains(Shop.ALMI))
+            .map(product -> product.getPrice())
+            .reduce((a, b) -> a + b);
+
+        sum.ifPresent(System.out::println);
     }
 }
